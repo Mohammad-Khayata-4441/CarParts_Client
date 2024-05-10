36a62b4fc93d0809dc748492ef92ebc72b2fd389
Mohammad Khayata <mohammadkhayata.gm@gmail.com>
2024-05-11 01:01:21 +0300

    new baseurl

--- src/app/config/app.config.ts
+++ src/app/config/app.config.ts
@@ -1,4 +1,4 @@
-const SERVER_URL = 'https://ahmadis01-001-site1.dtempurl.com';
+const SERVER_URL = 'https://ahmadis05-001-site1.btempurl.com/';
 // const SERVER_URL = 'https://localhost:7176';
 const API_URL = `${SERVER_URL}/api/`;
 const APP_CURRENCY = 'ل.س'
